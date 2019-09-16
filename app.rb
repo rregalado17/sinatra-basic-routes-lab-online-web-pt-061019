32e41ee8f98e55337014e57427a14be5afcbf3ca
Robinson Regalado <rregalado17@gmail.com>
2019-09-16 04:48:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/name" do 
+    "My name is Robinson Regalado"
+  end
+  
+  get "/hometown" do 
+    "My hometown is New York City"
+  end
+  
+  get "/favorite-song" do 
+    "My favorite song is Hello"
+  end
+
 end
